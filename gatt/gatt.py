9f78557d1a3874009db07d80c2292bf01efb64ec
developer-at-speer <developer@speer.io>
2022-08-31 16:12:54 -0400

    chang read

--- gatt/gatt.py
+++ gatt/gatt.py
@@ -39,7 +39,7 @@ class RxCharacteristic(Characteristic):
         print('write value has been triggered')
         dm = bytearray(value).decode()
         logger.warning(dm)
-        cmd = subprocess.check_output(['autopi','crypto.query','ethereum_address'])
+        cmd = subprocess.check_output(['sudo','autopi','crypto.query','ethereum_address'])
         logger.warning(cmd)
         logger.warning('cmd: ')
         logger.warning(str.encode(cmd))
